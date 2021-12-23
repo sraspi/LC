b6ae7e4289a17ac7399f5b1ba1920740de3797be
Stefan Taubert-Apweiler <stefan.taubert.apweiler@gmail.com>
2021-12-23 20:02:58 +0000

    lC1.6.py update

---
+++ LC-launcher.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#LC-laucher.sh
+cd /
+cd /home/pi/LC
+python3 LC1.6.py
---
+++ bootjobs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#bootjobs.sh
+cd
+cd /home/pi/noip-2.1.9-1
+sudo noip2
+cd
+sleep 6
+sudo mount -a
+echo "NAS mounted"
+cd /home/pi/LC/
+#sh filecopy.sh
+#rm logfile.txt
+sh LC-launcher.sh
