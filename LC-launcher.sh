e66ea30bc0da7063b82e624efbdd6498d1fda945
Stefan Taubert-Apweiler <stefan.taubert.apweiler@gmail.com>
2022-05-08 20:03:25 +0200

    LC4.9 bugfixes, errors to LC.log

--- LC-launcher.sh
+++ LC-launcher.sh
@@ -2,4 +2,4 @@
 #LC-laucher.sh
 cd /
 cd /home/pi/LC
-python3 LC4.8.py
+python3 LC4.9.py
--- mail_lc_status.py
+++ mail_lc_status.py
@@ -1,57 +1,65 @@
 def lc_mail():                       #E-Mail an sraspi21@gmail.com:
+    try:  
+        import urllib
+        import smtplib, ssl
+        import email
+        from email import encoders
+        from email.mime.base import MIMEBase
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+
+        Inhalt = ("LC.log")
+        Betreff = str("loop-control-log")
+        sender_email = "sraspi21@gmail.com"
+        receiver_email = "sraspi21@gmail.com"
+        password = "1rwnqyynanebneqbj"
+        #password = input("Type your password and press enter:")
+
+        # Create a multipart message and set headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = Betreff
+        message["Bcc"] = receiver_email  # Recommended for mass emails
+
+        # Add body to email
+        message.attach(MIMEText(Inhalt, "plain"))
+
+        filename = "/home/pi/NAS/LC.log" # In same directory as script
+           
+        # Open PDF file in binary mode  
+        with open(filename, "rb") as attachment:
+            #Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+            # Encode file in ASCII characters to send by email    
+            encoders.encode_base64(part)
+
+
+                
+            # Add header as key/value pair to attachment part
+            part.add_header("Content-Disposition", "attachment; filename=LC.log",)
+            # Add attachment to message and convert message to string
+            message.attach(part)
+            text = message.as_string()
+
+            # Log in to server using secure context and send email
+            context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, text)
+
+        print("LC-status sent")
+    
+    except:
+        e = sys.exc_info()[1]
+        print("error", e)
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        f = open("/home/pi/data/LC.log", "a")
+        f.write( '\n' + timestr + "LC-status-mail error:  " + str(e) + '\n')
+        f.close() 
+        
   
-    import urllib
-    import smtplib, ssl
-    import email
-    from email import encoders
-    from email.mime.base import MIMEBase
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-
-    Inhalt = ("LC.log")
-    Betreff = str("loop-control-log")
-    sender_email = "sraspi21@gmail.com"
-    receiver_email = "sraspi21@gmail.com"
-    password = "rwnqyynanebneqbj"
-    #password = input("Type your password and press enter:")
-
-    # Create a multipart message and set headers
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = Betreff
-    message["Bcc"] = receiver_email  # Recommended for mass emails
-
-    # Add body to email
-    message.attach(MIMEText(Inhalt, "plain"))
-
-    filename = "/home/pi/NAS/LC.log" # In same directory as script
-       
-    # Open PDF file in binary mode  
-    with open(filename, "rb") as attachment:
-        #Add file as application/octet-stream
-        # Email client can usually download this automatically as attachment
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-
-        # Encode file in ASCII characters to send by email    
-        encoders.encode_base64(part)
-
-
-            
-        # Add header as key/value pair to attachment part
-        part.add_header("Content-Disposition", "attachment; filename=LC.log",)
-        # Add attachment to message and convert message to string
-        message.attach(part)
-        text = message.as_string()
-
-        # Log in to server using secure context and send email
-        context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, text)
-
-    print("LC-status sent")
-  
-
-
